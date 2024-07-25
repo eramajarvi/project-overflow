c77a468da95ea7dc1f3ff76c8204c183d27688d0
James <danjame@hotmail.com>
2024-07-25 04:04:40 -0500

    refactoring for not needed components

--- src/components/ShaderWrapper.jsx
+++ src/components/ShaderWrapper.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useStore } from "@nanostores/react";
-import { isLoggedin } from "../store";
+import { isLoggedin } from "../store.js";
 
 import Shader from "./Shader.jsx";
 
