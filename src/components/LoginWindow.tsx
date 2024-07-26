7610b6846f1ccc4aa75977c23c0f665fce68a5f1
James <danjame@hotmail.com>
2024-07-26 06:25:50 -0500

    hot fix for password icon

--- src/components/LoginWindow.tsx
+++ src/components/LoginWindow.tsx
@@ -3,7 +3,7 @@ import "../styles/global.css";
 
 import React, { useState } from "react";
 import Draggable from "react-draggable";
-import WarningWindow from "../assets/exclamation.png";
+import PasswordIcon from "../assets/password.png";
 
 
 function LoginWindow({ authProps, waitingProps, visibilityProps, shaderVisibility }) {
@@ -38,7 +38,7 @@ function LoginWindow({ authProps, waitingProps, visibilityProps, shaderVisibilit
           <div className="place-content-start mt-1">
             <img
               className="pixelated h-12 w-12"
-              src={WarningWindow.src}
+              src={PasswordIcon.src}
               alt="Password icon to log in into Windows"
             />
           </div>
