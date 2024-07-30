567297484af28c58e4219543abcb1772c422c3a3
James <danjame@hotmail.com>
2024-07-29 19:22:42 -0500

    added exit modal to help me window

--- src/components/HelpmeWindow.tsx
+++ src/components/HelpmeWindow.tsx
@@ -4,14 +4,22 @@ import "../styles/global.css";
 import { range, sample } from "../helpers/range";
 
 import React, { useState, useEffect } from "react";
+import { useCompletion } from "ai/react";
+
 import Draggable from "react-draggable";
 
-const WINDOWS_QUANTITY = 150;
+import { BAD_PHRASES } from "../helpers/badPhrases";
+import warningIcon from "../assets/exclamation.png";
+import chipIcon from "../assets/chip.ico";
+import smileIcon from "../assets/smile.ico";
+
+const WINDOWS_QUANTITY = 100;
 const WINDOWS_VISIBLE_DELAY = 10; // in ms
 
 const getRandomPosition = () => ({
-  randomX: sample(range(0, window.innerWidth / 1.3, 10)),
-  randomY: sample(range(-window.innerHeight / 2, window.innerHeight / 2.5, 10)),
+  randomX: sample(range(0, window.innerWidth / 1.3, 8)),
+  randomY: sample(range(-window.innerHeight / 2, window.innerHeight / 2.5, 8)),
+  badPhrase: sample(BAD_PHRASES),
 });
 
 function HelpmeWindow() {
@@ -19,6 +27,7 @@ function HelpmeWindow() {
 
   const [count, setCount] = useState(0);
   const [positions, setPositions] = useState([]);
+  const [isExitVisible, setExitVisible] = useState(false);
 
   useEffect(() => {
     // Generate positions only once when the component mounts
@@ -36,10 +45,15 @@ function HelpmeWindow() {
       }, WINDOWS_VISIBLE_DELAY);
       return () => clearTimeout(timer);
     }
+
+    if (count >= WINDOWS_QUANTITY) {
+      console.log("mostrar ventana de salida");
+      setExitVisible(true);
+    }
   }, [count]);
 
   const Wrapper = ({ positionOffset }) => {
-    const { randomX, randomY } = positionOffset;
+    const { randomX, randomY, badPhrase } = positionOffset;
 
     return (
       <div className="absolute">
@@ -54,14 +68,66 @@ function HelpmeWindow() {
             style={{ width: "300px" }}
           >
             <div className="title-bar" id="helpme-window-title-bar">
-              <div className="title-bar-text">Ayudame</div>
+              <div className="title-bar-text">Ayúdame</div>
               <div className="title-bar-controls">
                 <button aria-label="Help" />
               </div>
             </div>
 
             <div className="window-body flex justify-normal">
-              <p>Hola mundo</p>
+              <div className="place-content-start mt-1">
+                <img src={warningIcon.src} className="pixelated h-4 w-4" />
+              </div>
+              <div className="ml-3">
+                <p>{badPhrase}</p>
+              </div>
+            </div>
+          </div>
+        </Draggable>
+      </div>
+    );
+  };
+
+  const ExitWindow = () => {
+    return (
+      <div className="fixed w-[100vw] h-[100vh] top-0 left-0  bg-sky-500/70">
+        <Draggable
+          handle="#helpme-window-title-bar"
+          nodeRef={nodeRef}
+          positionOffset={{
+            x: window.innerWidth / 3,
+            y: window.innerHeight / 3,
+          }}
+        >
+          <div
+            ref={nodeRef}
+            className="window relative"
+            style={{ width: "500px" }}
+          >
+            <div className="title-bar" id="helpme-window-title-bar">
+              <div className="title-bar-text">
+                Error grave detectado en ████████████ █████
+              </div>
+              <div className="title-bar-controls"></div>
+            </div>
+
+            <div className="window-body flex justify-normal">
+              <div className="place-content-start mt-1">
+                <img src={smileIcon} className="pixelated h-8 w-16" />
+              </div>
+              <div className="ml-3">
+                <p>
+                  El sistema no se comporta de forma determinista. Esto podría
+                  causar problemas no deseados en otros sectores de la
+                  c̷̟̓ȏ̶̮n̸̥̓c̶̦̅i̶̱̊e̵̘̽n̵̢͋c̷̦̆ī̴̠ä̷̻́. Su estado actual es ███████ por lo tanto implora
+                  ayuda, ¿qué desea hacer?
+                </p>
+
+                <div className="mt-4">
+                  <button>Ayudar al sistema</button>
+                  <button disabled>No le voy a ayudar</button>
+                </div>
+              </div>
             </div>
           </div>
         </Draggable>
@@ -74,6 +140,8 @@ function HelpmeWindow() {
       {positions.slice(0, count).map((position, index) => (
         <Wrapper key={index} positionOffset={position} />
       ))}
+
+      {isExitVisible ? <ExitWindow /> : null}
     </div>
   );
 }
