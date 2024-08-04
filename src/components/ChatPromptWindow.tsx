19b0c5394c1bbef3039648a509d385d1329253bf
James <danjame@hotmail.com>
2024-08-04 03:41:01 -0500

    added logic to display sketches after each prompt

--- src/components/ChatPromptWindow.tsx
+++ src/components/ChatPromptWindow.tsx
@@ -4,7 +4,21 @@ import "../styles/global.css";
 import React, { useState, useEffect } from "react";
 import Draggable from "react-draggable";
 
+// Credits scene
 import MathTextbookSketchWrapper from "./sketches/MathTexbookSketchWrapper";
+
+// Other components
+import BreathSketchWrapper from "./sketches/BreathSketchWrapper";
+import CardiodSketchWrapper from "./sketches/CardiodSketchWrapper";
+import CilinderSketchWrapper from "./sketches/CilinderSketchWrapper";
+import LoudSketchWrapper from "./sketches/LoudSketchWrapper";
+import MalaguaSketchWrapper from "./sketches/MalaguaSketchWrapper";
+import PinkSketchWrapper from "./sketches/PinkSketchWrapper";
+import SpyderWebSketchWrapper from "./sketches/SpyderWebSketchWrapper";
+import WordCloudSketchWrapper from "./sketches/WordCloudSketchWrapper";
+import WordSaladSketchWrapper from "./sketches/WordSaladSketchWrapper";
+import XubitosSketchWrapper from "./sketches/XubitosSketchWrapper";
+import YarnSketchWrapper from "./sketches/YarnSketchWrapper";
 
 import { useChat } from "ai/react";
 
@@ -14,23 +28,139 @@ function ChatPromptWindow({ SettingsWindowVisibility }) {
 
   const { messages, input, setInput, append } = useChat();
 
-  const [count, setCount] = React.useState(3);
+  const [count, setCount] = React.useState(12);
   const [isUserLimited, setIsUserLimited] = React.useState(false);
   const [isEndReached, setIsEndReached] = React.useState(false);
+  const [chosenNumbers, setChosenNumbers] = React.useState([]);
 
   const { isSettingsWindowOpened, setIsSettingsWindowOpened } =
     SettingsWindowVisibility;
 
+  // Managing visibility for other window sketches
+  const [visibleSketch, setVisibleSketch] = useState(null);
+
   useEffect(() => {
+    if (count > 0) {
+      let availableNumbers = [...Array(12).keys()]
+        .map((n) => n + 1)
+        .filter((n) => !chosenNumbers.includes(n));
+
+      if (availableNumbers.length > 0) {
+        const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+        const chosenNumber = availableNumbers[randomIndex];
+        setChosenNumbers([...chosenNumbers, chosenNumber]);
+        console.log("Chosen number:", chosenNumber);
+        handleComponentVisibility(chosenNumber);
+      } else {
+        // Reset chosenNumbers if all numbers have been chosen
+        setChosenNumbers([]);
+      }
+    }
     if (count === 0) {
       setIsUserLimited(true);
       setIsEndReached(true);
+      setVisibleSketch(0);
     }
   }, [count]);
+
+  function handleComponentVisibility(chosenNumber) {
+    switch (chosenNumber) {
+      case 1:
+        // Logic for chosenNumber = 1
+        console.log("Handling visibility for number 1");
+        setVisibleSketch(1);
+        break;
+
+      case 2:
+        // Logic for chosenNumber = 2
+        console.log("Handling visibility for number 2");
+        setVisibleSketch(2);
+        break;
+
+      case 3:
+        // Logic for chosenNumber = 3
+        console.log("Handling visibility for number 3");
+        setVisibleSketch(3);
+        break;
+
+      case 4:
+        // Logic for chosenNumber = 4
+        console.log("Handling visibility for number 4");
+        setVisibleSketch(4);
+        break;
+
+      case 5:
+        // Logic for chosenNumber = 5
+        console.log("Handling visibility for number 5");
+        setVisibleSketch(5);
+        break;
+
+      case 6:
+        // Logic for chosenNumber = 6
+        console.log("Handling visibility for number 6");
+        setVisibleSketch(6);
+        break;
+
+      case 7:
+        // Logic for chosenNumber = 7
+        console.log("Handling visibility for number 7");
+        setVisibleSketch(7);
+
+        break;
+
+      case 8:
+        // Logic for chosenNumber = 3
+        console.log("Handling visibility for number 8");
+        setVisibleSketch(8);
+        break;
+
+      case 9:
+        // Logic for chosenNumber = 3
+        console.log("Handling visibility for number 9");
+        setVisibleSketch(9);
+        break;
+
+      case 10:
+        // Logic for chosenNumber = 3
+        console.log("Handling visibility for number 10");
+        setVisibleSketch(10);
+        break;
+
+      case 11:
+        // Logic for chosenNumber = 3
+        console.log("Handling visibility for number 11");
+        setVisibleSketch(11);
+
+        break;
+
+      case 12:
+        // Logic for chosenNumber = 12
+        console.log("Handling visibility for number 12");
+        setVisibleSketch(12);
+        break;
+
+      default:
+        // Default case if needed
+        console.log("Number not handled");
+    }
+  }
 
   return (
     <>
       {isEndReached ? <MathTextbookSketchWrapper /> : null}
+
+      {visibleSketch === 1 && <BreathSketchWrapper />}
+      {visibleSketch === 2 && <CardiodSketchWrapper />}
+      {visibleSketch === 3 && <CilinderSketchWrapper />}
+      {visibleSketch === 4 && <LoudSketchWrapper />}
+      {visibleSketch === 5 && <MalaguaSketchWrapper />}
+      {visibleSketch === 6 && <PinkSketchWrapper />}
+      {visibleSketch === 7 && <SpyderWebSketchWrapper />}
+      {visibleSketch === 8 && <WordCloudSketchWrapper />}
+      {visibleSketch === 9 && <WordSaladSketchWrapper />}
+      {visibleSketch === 10 && <XubitosSketchWrapper />}
+      {visibleSketch === 11 && <YarnSketchWrapper />}
+      {visibleSketch === 12 && <BreathSketchWrapper />}
 
       <div className="absolute">
         <Draggable
