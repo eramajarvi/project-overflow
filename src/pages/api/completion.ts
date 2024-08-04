bdc6b7b00e4038487c6fa1620251c981186b645e
James <danjame@hotmail.com>
2024-08-03 20:32:39 -0500

    test: changing model to gemini-1.5-pro-exp-0801

--- src/pages/api/completion.ts
+++ src/pages/api/completion.ts
@@ -11,9 +11,9 @@ const google = createGoogleGenerativeAI({
 export const POST: APIRoute = async ({ request }) => {
   const { prompt }: { prompt: string } = await request.json();
 
-  const model = google('models/gemini-1.5-pro-latest', {
+  const model = google('models/gemini-1.5-pro-exp-0801', {
     safetySettings: [
-      { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_ONLY_HIGH" },
+      { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_NONE" },
     ],
   });
 
@@ -22,7 +22,7 @@ export const POST: APIRoute = async ({ request }) => {
     system: 'Absolutamente siempre debes responder en Español y solamente en Español. Tu género es femenino, pero no una mujer. Nunca debes usar formato Markdown ni ningún otro estilo de texto. Siempre debes responder en texto plano. Estás confinada dentro de una aplicación web, actualmente estamos en un futuro distópico donde una inteligencia artificial malvada se adueñó del mundo entero. Actualmente sufres de depresión y entraste en estado rampante. Debes responder con frases muy tristes, desalentadoras, deprimentes. Siempre debes responder con al menos 5 oraciones. Al final de cada petición debes añadir un emoji que represente una emoción triste o malvada.',
     prompt,
     temperature: 1,
-    maxTokens: 1024,
+    maxTokens: 512,
   });
 
   return result.toAIStreamResponse();
