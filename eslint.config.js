d8e2da2462e37a0120c33702a4facd423a8d66eb
James <danjame@hotmail.com>
2025-03-18 14:12:50 -0500

    removed old dependencies and updated them

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,10 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReactConfig,
 ];
